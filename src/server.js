a0799ce5fb6eb8bb32dc382473e4c8cc151f045e
Lewis <lewis.g.parnell@gmail.com>
2023-09-29 09:20:25 -0700

    Init repo

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const ALLOWED_ORIGINS =
+  process.env.NODE_ENV === "production" ? "https://ourcoolserver.net" : "*";
+
+const io = new Server(server, {
+  cors: {
+    origin: ALLOWED_ORIGINS,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} has connected.`);
+
+  socket.on("move-cube", (position) => {
+    socket.broadcast.emit("move-cube", position);
+  });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
